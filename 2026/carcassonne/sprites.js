c3831a522de8eeb83fdffbb23b1952dd77414cef
Detmar <detmar@peterke.com>
2026-08-14 18:24:47 -0700

    carcassonne fun

--- 2026/carcassonne/sprites.js
+++ 2026/carcassonne/sprites.js
@@ -2,17 +2,53 @@ export default class Sprites {
   constructor() {
     this.tileSize = 128;
     this.tileImages = {};
+    this.columns = 8;
+    this.rows = 8;
+
+    this.sheet = new Image();
+    this.sheet.src = 'data/c03.png';
   }
 
   getImage(params = {}, x = 0, y = 0) {
   }
 
-  draw(ctx, x, y) {
+  draw(ctx, n, r) {
+    this._drawSprite(ctx, 0, 0, n, r);
+    //this._drawPlaceholder(ctx, 0, 0);
+  }
+
+  _getSource(n) {
+    n = n % 24;
+    const col = n % 6 + 1;
+    const row = Math.floor(n / 6) + 1;
+    const sx = col * this.tileSize;
+    const sy = row * this.tileSize;
+    return [sx, sy, this.tileSize, this.tileSize];
+  }
+
+  _drawImage(ctx, x, y) {
+    let n = Math.floor(Math.random() * 64);
+    let r = Math.floor(Math.random() * 4);
+    this._drawSprite(ctx, x, y, n, r);
+  }
+
+  _drawSprite(ctx, x, y, n, r) {
+    const [sx, sy, sw, sh] = this._getSource(n);
+    const cx = x + sw / 2;
+    const cy = y + sh / 2;
+    ctx.save();
+    ctx.translate(cx, cy);
+    ctx.rotate((r * Math.PI) / 2);
+    ctx.drawImage(this.sheet, sx, sy, sw, sh, -sw / 2, -sh / 2, sw, sh);
+    ctx.restore();
+  }
+
+  _drawPlaceholder(ctx, x, y) {
     const px = 0;
     const py = 0;
 
     ctx.fillStyle = '#fff';
-    ctx.fillRect(px, py, this.tileSize, this.tileSize);
+    //ctx.fillRect(px, py, this.tileSize, this.tileSize);
 
     ctx.strokeStyle = '#000';
     ctx.lineWidth = 1;
@@ -22,4 +58,6 @@ export default class Sprites {
     ctx.font = '12px monospace';
     ctx.fillText(`${x},${y}`, px + 5, py + 20);
   }
+
+
 }
--- 2026/carcassonne/view.js
+++ 2026/carcassonne/view.js
@@ -1,6 +1,18 @@
 import Sprites from './sprites.js';
 import Finger from './finger.js';
 
+class Random {
+  constructor(seed) {
+    this.seed = Math.floor(Math.abs(seed));
+  }
+
+  next(n) {
+    this.seed = (this.seed * 9301 + 49297) % 233280;
+    return Math.floor(this.seed / 233280 * n);
+  }
+}
+
+
 export default class View {
   constructor(root) {
     this.canvas = document.createElement('canvas');
@@ -19,6 +31,8 @@ export default class View {
 
     this.sprites = new Sprites();
     this.finger = new Finger(this.canvas);
+
+    this.drag = [];
   }
 
   start() {
@@ -29,12 +43,14 @@ export default class View {
 
   raf() {
     const now = performance.now();
-    const dt = (now - this.lastFrameTime) / 1000;
-    this.lastFrameTime = now;
+    let dt = (now - this.lastFrameTime) / 1000;
 
-    this.work(dt);
-    this.draw();
-
+    if (dt > 0.0) {
+      this.lastFrameTime = now;
+      dt = Math.min(dt, 0.1);
+      this.work(dt);
+      this.draw();
+    }
     if (this.running) {
       requestAnimationFrame(() => this.raf());
     }
@@ -52,31 +68,60 @@ export default class View {
     }
 
     this.finger.work(dt);
+    let [dx, dy] = [0, 0];
+    if (this.finger.dragging) {
+      [dx, dy] = [this.finger.dx / dt, this.finger.dy / dt];
+      console.log(`ddd0 ${dx},${dy}`);
+      if (this.drag.length > 2) {
+        this.drag.shift();
+      }
+      this.drag.push([dx, dy]);
+    }
+
+    if (this.finger.isIdle) {
+      if (this.drag.length > 0) {
+        console.log(`ddd1 drag ${this.drag}`);
+        this.inertia = this.drag.reduce((acc, [dx, dy]) => {
+          return [acc[0] + dx, acc[1] + dy];
+        }, [0, 0]);
+        this.inertia = [this.inertia[0] / this.drag.length, this.inertia[1] / this.drag.length];
+        console.log(`ddd2 inertia ${this.inertia}`);
+        this.drag = [];
+      }
+      if (this.inertia) {
+        dx = this.inertia[0] * dt;
+        dy = this.inertia[1] * dt;
+      }
+    }
+    else {
+      this.inertia = null;
+    }
+
     this.cameraX = this.finger.x;
     this.cameraY = this.finger.y;
     this.zoom = 1;
+
+    this._rebuildBoard();
   }
 
   draw() {
     this.ctx.fillStyle = '#333';
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
     this.ctx.save();
     this.ctx.scale(this.zoom, this.zoom);
 
-    let topleft = this._deviceToTile([0, 0]);
-    console.log('topleft', topleft);
-    let bottomright = this._deviceToTile([this.canvas.width, this.canvas.height]);
-
-    for (let row = Math.ceil(topleft[1]); row < Math.floor(bottomright[1]); row++) {
-      for (let col = Math.ceil(topleft[0]); col < Math.floor(bottomright[0]); col++) {
-        this.ctx.save();
-        this.ctx.translate(col * this.sprites.tileSize - this.cameraX, row * this.sprites.tileSize - this.cameraY);
-        this.sprites.draw(this.ctx, col, row);
-        this.ctx.restore();
-      }
+    for (let key in this.board) {
+      let tile = this.board[key];
+      let col = tile.col;
+      let row = tile.row;
+      this.ctx.save();
+      this.ctx.translate(col * this.sprites.tileSize - this.cameraX, row * this.sprites.tileSize - this.cameraY);
+      let rand = new Random(col * 1000 + row);
+      let n = rand.next(64);
+      let r = rand.next(4);
+      this.sprites.draw(this.ctx, n, r);
+      this.ctx.restore();
     }
-
     this.ctx.restore();
   }
 
@@ -88,5 +133,54 @@ export default class View {
 
   _deviceToWorld(device) {
     return [ (device[0] + this.cameraX) / this.zoom, (device[1] + this.cameraY) / this.zoom ];
+  }
+
+  _rebuildBoard() {
+    this.board ||= {};
+    let missing = null;
+
+    let topleft = this._deviceToTile([0, 0]);
+    let bottomright = this._deviceToTile([this.canvas.width, this.canvas.height]);
+
+    let padding = 1;
+    let left = Math.ceil(topleft[0]) - padding;
+    let right = Math.floor(bottomright[0]) + padding;
+    let top = Math.ceil(topleft[1]) - padding;
+    let bottom = Math.floor(bottomright[1]) + padding;
+
+    let found = [];
+    for (let row = top; row < bottom; row++) {
+      for (let col = left; col < right; col++) {
+        let key = `${col},${row}`;
+        if (this.board[key]) {
+          found.push(key);
+        }
+        else {
+          this.board[key] = true;
+          missing ||= [];
+          missing.push({
+            key: key,
+            col: col,
+            row: row,
+          });
+        }
+      }
+    }
+
+    if (!missing && found.length == Object.keys(this.board).length) {
+      return;
+    }
+
+    let newBoard = {};
+    for (let key of found) {
+      newBoard[key] = this.board[key];
+    }
+    if (missing) {
+      for (let tile of missing) {
+        newBoard[tile.key] = tile;
+      }
+    }
+    console.log(`bbb ${found.length} ${missing?.length || 0} ${Object.keys(this.board).length}`);
+    this.board = newBoard;
   }
 }
